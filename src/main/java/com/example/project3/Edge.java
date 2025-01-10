f00809a45a22eb7e666bba34bcd7e3b169632d65
Osama Abujarad <1202883@student.birzeit.edu>
2025-01-10 02:42:47 +0200

    Update Edge.java

--- src/main/java/com/example/project3/Edge.java
+++ src/main/java/com/example/project3/Edge.java
@@ -1,4 +1,29 @@
 package com.example.project3;
 
+
 public class Edge {
+    private Point destination;
+    private double cost;
+    private int time;
+
+    public Edge(Point destination, double cost, int time) {
+        this.destination = destination;
+        this.cost = cost;
+        this.time = time;
+    }
+
+    public Point getDestination() {
+        return destination;
+    }
+    public double getCost() {
+        return cost;
+    }
+    public int getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge to " + destination.getName() + " [cost=" + cost + ", time=" + time + "]";
+    }
 }
