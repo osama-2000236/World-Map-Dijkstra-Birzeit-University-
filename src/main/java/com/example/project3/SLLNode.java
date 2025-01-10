8cae7f3ce2990e44798bc22ea19de0339b0be43c
Osama Abujarad <1202883@student.birzeit.edu>
2025-01-10 02:41:46 +0200

    Update SLLNode.java

--- src/main/java/com/example/project3/SLLNode.java
+++ src/main/java/com/example/project3/SLLNode.java
@@ -1,4 +1,33 @@
 package com.example.project3;
 
-public class SLLNode {
+
+public class SLLNode<T> {
+    private T element;
+    private SLLNode<T> next;
+
+    public SLLNode(T element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public SLLNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(SLLNode<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
 }
